cba475cc53249f7d95767b8ad089c9129728f1c3
igorshyian <yourbestigor@gmail.com>
2022-12-28 19:22:56 +0200

    WIP: New Beneficiary screen UI

--- app/src/main/java/com/topiichat/app/features/new_beneficiary/presentation/NewBeneficiaryFragment.kt
+++ app/src/main/java/com/topiichat/app/features/new_beneficiary/presentation/NewBeneficiaryFragment.kt
@@ -1,8 +1,12 @@
 package com.topiichat.app.features.new_beneficiary.presentation
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.topiichat.app.R
 import com.topiichat.app.databinding.FragmentNewBeneficiaryBinding
+import com.topiichat.core.extension.showSelectorDialog
 import com.topiichat.core.presentation.platform.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,4 +17,14 @@ class NewBeneficiaryFragment : BaseFragment<FragmentNewBeneficiaryBinding>(), IN
         FragmentNewBeneficiaryBinding.inflate(inflater, container, false)
 
     override fun onVisibilityLoader(isVisibleLoader: Boolean) = Unit
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
+        textCountry.editText.isEnabled = false
+        textCountry.setOnClickListener {
+            val options = listOf("Ukraine", "GT")
+            showSelectorDialog(getString(R.string.address_address_hint), options) { _, position ->
+                binding.textCountry.text = options[position]
+            }
+        }
+    }
 }
